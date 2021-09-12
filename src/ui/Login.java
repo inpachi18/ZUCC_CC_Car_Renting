f394a8f0e1e8c4ed75d3e6b71d58ea73062e1c9c
inpachi18 <swjh2000@foxmail.com>
2021-09-12 09:00:50 +0800

    修改

--- src/ui/Login.java
+++ src/ui/Login.java
@@ -1,157 +1,137 @@
 /*
- * Created by JFormDesigner on Sat Sep 11 14:42:01 CST 2021
+ * Created by JFormDesigner on Sun Sep 12 08:54:20 CST 2021
  */
 
 package ui;
 
-import control.StfManager;
-import util.BaseException;
-
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.GroupLayout;
 
 /**
  * @author inpachi
  */
-public class Login extends JFrame {
-    public Login(Frame f, String s, boolean b) {
+public class Login extends JDialog {
+    public Login(Frame f, String s, Boolean b) {
         super(f, s, b);
         initComponents();
     }
 
-
-    private void registerActionPerformed(ActionEvent e) {
+    private void btnregActionPerformed() {
         // TODO add your code here
     }
 
-    private void stfLoginActionPerformed(ActionEvent e) {
-        String ID = this.ID.getText();
-        String pwd = new String(this.Pwd.getPassword());
-        try {
-            StfManager a = new StfManager();
-            StfManager.currentStf = a.login(ID, pwd);
-        } catch (BaseException e1) {
-            JOptionPane.showMessageDialog(null, e1.getMessage());
-            return;
-        }
-        this.setVisible(false);
-    }
-
-    private void userLoginActionPerformed(ActionEvent e) {
+    private void btnstfActionPerformed() {
         // TODO add your code here
     }
 
-    private void button1ActionPerformed(ActionEvent e) {
+    private void btnuserActionPerformed() {
         // TODO add your code here
     }
-
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-        登陆 = new JFrame();
-        label2 = new JLabel();
-        ID = new JTextField();
-        label3 = new JLabel();
-        Pwd = new JPasswordField();
-        register = new JButton();
-        stfLogin = new JButton();
-        userLogin = new JButton();
+        passwordField1 = new JPasswordField();
+        label5 = new JLabel();
+        label4 = new JLabel();
+        textField1 = new JTextField();
         label1 = new JLabel();
+        btnreg = new JButton();
+        btnstf = new JButton();
+        btnuser = new JButton();
 
-        //======== 登陆 ========
-        {
-            var 登陆ContentPane = 登陆.getContentPane();
+        //======== this ========
+        var contentPane = getContentPane();
 
-            //---- label2 ----
-            label2.setText("\u7528\u6237\u7f16\u53f7:");
+        //---- label5 ----
+        label5.setText("\u5bc6\u7801");
 
-            //---- label3 ----
-            label3.setText("\u5bc6\u7801\uff1a");
+        //---- label4 ----
+        label4.setText("\u7528\u6237\u540d");
 
-            //---- register ----
-            register.setText("\u6ce8\u518c");
-            register.addActionListener(e -> {
-			button1ActionPerformed(e);
-			registerActionPerformed(e);
-		});
+        //---- label1 ----
+        label1.setText("\u6b22\u8fce\u6765\u5230\u79df\u8f66\u7ba1\u7406\u7cfb\u7edf");
+        label1.setHorizontalAlignment(SwingConstants.CENTER);
 
-            //---- stfLogin ----
-            stfLogin.setText("\u5458\u5de5\u767b\u9646");
-            stfLogin.addActionListener(e -> stfLoginActionPerformed(e));
+        //---- btnreg ----
+        btnreg.setText("\u6ce8\u518c");
+        btnreg.addActionListener(e -> btnregActionPerformed());
 
-            //---- userLogin ----
-            userLogin.setText("\u7528\u6237\u767b\u9646");
-            userLogin.addActionListener(e -> userLoginActionPerformed(e));
+        //---- btnstf ----
+        btnstf.setText("\u5458\u5de5\u767b\u9646");
+        btnstf.addActionListener(e -> btnstfActionPerformed());
 
-            //---- label1 ----
-            label1.setText("\u6b22\u8fce\u6765\u5230\u79df\u8f66\u7ba1\u7406\u7cfb\u7edf");
-            label1.setHorizontalAlignment(SwingConstants.CENTER);
+        //---- btnuser ----
+        btnuser.setText("\u7528\u6237\u767b\u9646");
+        btnuser.addActionListener(e -> btnuserActionPerformed());
 
-            GroupLayout 登陆ContentPaneLayout = new GroupLayout(登陆ContentPane);
-            登陆ContentPane.setLayout(登陆ContentPaneLayout);
-            登陆ContentPaneLayout.setHorizontalGroup(
-                登陆ContentPaneLayout.createParallelGroup()
-                    .addGroup(登陆ContentPaneLayout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(登陆ContentPaneLayout.createParallelGroup()
-                            .addComponent(label1, GroupLayout.PREFERRED_SIZE, 535, GroupLayout.PREFERRED_SIZE)
-                            .addGroup(登陆ContentPaneLayout.createSequentialGroup()
-                                .addGap(51, 51, 51)
-                                .addGroup(登陆ContentPaneLayout.createParallelGroup()
-                                    .addGroup(登陆ContentPaneLayout.createSequentialGroup()
-                                        .addComponent(label2, GroupLayout.PREFERRED_SIZE, 119, GroupLayout.PREFERRED_SIZE)
-                                        .addGap(18, 18, 18)
-                                        .addComponent(ID, GroupLayout.PREFERRED_SIZE, 285, GroupLayout.PREFERRED_SIZE))
-                                    .addGroup(登陆ContentPaneLayout.createSequentialGroup()
-                                        .addComponent(label3, GroupLayout.PREFERRED_SIZE, 119, GroupLayout.PREFERRED_SIZE)
-                                        .addGap(18, 18, 18)
-                                        .addComponent(Pwd, GroupLayout.PREFERRED_SIZE, 285, GroupLayout.PREFERRED_SIZE))
-                                    .addGroup(登陆ContentPaneLayout.createSequentialGroup()
-                                        .addComponent(register)
-                                        .addGap(18, 18, 18)
-                                        .addComponent(stfLogin)
-                                        .addGap(18, 18, 18)
-                                        .addComponent(userLogin)))))
-                        .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            );
-            登陆ContentPaneLayout.setVerticalGroup(
-                登陆ContentPaneLayout.createParallelGroup()
-                    .addGroup(登陆ContentPaneLayout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(label1, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addGroup(登陆ContentPaneLayout.createParallelGroup()
-                            .addComponent(label2, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
-                            .addComponent(ID, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-                        .addGap(18, 18, 18)
-                        .addGroup(登陆ContentPaneLayout.createParallelGroup()
-                            .addComponent(label3, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
-                            .addComponent(Pwd, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-                        .addGap(28, 28, 28)
-                        .addGroup(登陆ContentPaneLayout.createParallelGroup()
-                            .addComponent(register)
-                            .addGroup(登陆ContentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(stfLogin)
-                                .addComponent(userLogin)))
-                        .addContainerGap(40, Short.MAX_VALUE))
-            );
-            登陆.pack();
-            登陆.setLocationRelativeTo(登陆.getOwner());
-        }
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGroup(contentPaneLayout.createSequentialGroup()
+                    .addGroup(contentPaneLayout.createParallelGroup()
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                            .addContainerGap()
+                            .addComponent(label1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                            .addGroup(contentPaneLayout.createParallelGroup()
+                                .addGroup(contentPaneLayout.createSequentialGroup()
+                                    .addGap(38, 38, 38)
+                                    .addComponent(btnreg)
+                                    .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                                    .addComponent(btnstf)
+                                    .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                                    .addComponent(btnuser))
+                                .addGroup(contentPaneLayout.createSequentialGroup()
+                                    .addGap(51, 51, 51)
+                                    .addGroup(contentPaneLayout.createParallelGroup()
+                                        .addGroup(contentPaneLayout.createSequentialGroup()
+                                            .addComponent(label4, GroupLayout.PREFERRED_SIZE, 98, GroupLayout.PREFERRED_SIZE)
+                                            .addGap(18, 18, 18)
+                                            .addComponent(textField1, GroupLayout.PREFERRED_SIZE, 282, GroupLayout.PREFERRED_SIZE))
+                                        .addGroup(contentPaneLayout.createSequentialGroup()
+                                            .addComponent(label5, GroupLayout.PREFERRED_SIZE, 98, GroupLayout.PREFERRED_SIZE)
+                                            .addGap(18, 18, 18)
+                                            .addComponent(passwordField1, GroupLayout.PREFERRED_SIZE, 282, GroupLayout.PREFERRED_SIZE)))))
+                            .addGap(0, 40, Short.MAX_VALUE)))
+                    .addContainerGap())
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGroup(contentPaneLayout.createSequentialGroup()
+                    .addGap(23, 23, 23)
+                    .addComponent(label1, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
+                    .addGap(30, 30, 30)
+                    .addGroup(contentPaneLayout.createParallelGroup()
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                            .addGap(8, 8, 8)
+                            .addComponent(label4, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE))
+                        .addComponent(textField1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                    .addGap(12, 12, 12)
+                    .addGroup(contentPaneLayout.createParallelGroup()
+                        .addComponent(label5, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(passwordField1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                    .addGap(46, 46, 46)
+                    .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(btnreg)
+                        .addComponent(btnstf)
+                        .addComponent(btnuser))
+                    .addContainerGap(66, Short.MAX_VALUE))
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    private JFrame 登陆;
-    private JLabel label2;
-    private JTextField ID;
-    private JLabel label3;
-    private JPasswordField Pwd;
-    private JButton register;
-    private JButton stfLogin;
-    private JButton userLogin;
+    private JPasswordField passwordField1;
+    private JLabel label5;
+    private JLabel label4;
+    private JTextField textField1;
     private JLabel label1;
+    private JButton btnreg;
+    private JButton btnstf;
+    private JButton btnuser;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
